41ddcfc307a210199b77a387f14c3c535d101503
gabridguez <gabridguez@gmail.com>
2016-12-01 10:28:50 +0100

    Fixed errors on SwimmersDAO.
    Fixed errors on SwimmersDAO and added some enhancements to swimmers view, still not finished.

--- persistence/src/main/java/swim/daos/SwimmerDAO.java
+++ persistence/src/main/java/swim/daos/SwimmerDAO.java
@@ -12,27 +12,29 @@ import swim.entities.Swimmer;
 
 public class SwimmerDAO {
 
-	private static EntityManagerFactory emf;
-
+	//private static EntityManagerFactory emf;
+	private static EntityManager em;
+	
+	
 	public SwimmerDAO(EntityManager em) {
-		emf = Persistence.createEntityManagerFactory("swim-database");
-	}
-
+        //emf = Persistence.createEntityManagerFactory("swim-database");
+        this.em = em;
+}
 	public void createSwimmer(Swimmer s) {
-		swim.entities.TransactionUtils.doTransaction(emf, em -> {
+		swim.entities.TransactionUtils.doTransaction(em, em -> {
 			em.persist(s);
 		});
 	}
 
 	public Swimmer findSwimmer(int id) {
-		return emf.createEntityManager().find(Swimmer.class, id);
+		return this.em.find(Swimmer.class, id);
 	}
 
 	public void removeSwimmer(int id) {
 		Swimmer s = findSwimmer(id);
 		if (s != null) {
 			//emf.createEntityManager().remove(s);
-			swim.entities.TransactionUtils.doTransaction(emf, em->{
+			swim.entities.TransactionUtils.doTransaction(this.em, em->{
 				
 				em.remove(em.contains(s) ? s : em.merge(s));
 			});
@@ -40,7 +42,7 @@ public class SwimmerDAO {
 	}
 	
 	public HashSet<Swimmer> findAllSwimmers() {
-	    Query query = emf.createEntityManager().createQuery("SELECT e FROM Swimmer e");
+	    Query query = this.em.createQuery("SELECT e FROM Swimmer e");
 	    return new HashSet<Swimmer>(query.getResultList());
 	  }
 
